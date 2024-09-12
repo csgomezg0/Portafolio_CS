c253b0a5444d9bc75921875f96bb36a18a2bb1d0
stevens <stevens@stevens.myguest.virtualbox.org>
2024-09-11 19:40:13 -0500

    warnings arreglados

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../css/FileCSS.css';
 import { textos } from '../textos/textosVar.js'; // Importa la constante desde texto.js
+console.log("sdjosdnfjksdnsdfjksdfnsdfjknsdfjkn")
 
-const contactPage= ({}) => {
+console.log(textos.texto25)
+
+const contactPage= () => {
     return (
       <section id="contact">
         <h2 id="texto20">{textos.texto20}</h2>
         <p>
           <span id="texto24">{textos.texto24}</span>
-          <a id="texto21">{textos.texto21}</a>
+          <a id="texto21" href="https://github.com/csgomezg0">{textos.texto21}</a>
           <span id="texto25">{textos.texto25}</span>
-          <a id="texto22">{textos.texto22}</a>.
+          <a id="texto22" href="https://github.com/csgomezg0">{textos.texto22}</a>.
         </p>
       </section>
 
