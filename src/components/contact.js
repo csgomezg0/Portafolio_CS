aca59dd98d342df44252912fae65c5a985500c6a
stevens <stevens@stevens.myguest.virtualbox.org>
2024-09-14 12:40:31 -0500

    referencia linkedin

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ const contactPage= () => {
           <span id="texto24">{textos.texto24}</span>
           <a id="texto21" href="https://github.com/csgomezg0">{textos.texto21}</a>
           <span id="texto25">{textos.texto25}</span>
-          <a id="texto22" href="https://github.com/csgomezg0">{textos.texto22}</a>.
+          <a id="texto22" href="https://www.linkedin.com/in/carlos-gomez-0a1869174/">{textos.texto22}</a>.
         </p>
       </section>
 
